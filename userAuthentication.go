77d3f21fe724163b3cd4b42dd79fbdc10c772a22
williamneo <williamneokh@gmail.com>
2022-03-29 23:00:03 +0800

    Done activity #4 if else if else statement

--- userAuthentication.go
+++ userAuthentication.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Insert Code here
+
+	var userInputName string
+
+	fmt.Println("Please key in your username:")
+	_, _ = fmt.Scanln(&userInputName)
+
+	if userInputName == "Admin" {
+		fmt.Println("Welcome, Admin!")
+	} else if userInputName == "Robin" {
+		fmt.Println("Welcome!", userInputName)
+	} else if userInputName == "John" {
+		fmt.Println("Welcome!", userInputName)
+	} else {
+		fmt.Println("Merry Men")
+	}
+
 }
